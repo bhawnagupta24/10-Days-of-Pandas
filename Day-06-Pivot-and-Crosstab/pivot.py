4dd3ff806783f01e8040d3f513360dd51023a81e
Bhawna Gupta <bhawnasnbgupta77@gmail.com>
2026-06-01 14:24:50 +0530

    Day 06: Learned pivot tables and crosstab in Pandas

---
+++ Day-06-Pivot-and-Crosstab/pivot.py
@@ -0,0 +1,8 @@
+# Day 06 - Pivot Tables and Crosstab
+
+Topics covered:
+- Creating pivot tables
+- Using crosstab()
+- Working with date columns
+- Extracting months and quarters
+- Sales data analysis
